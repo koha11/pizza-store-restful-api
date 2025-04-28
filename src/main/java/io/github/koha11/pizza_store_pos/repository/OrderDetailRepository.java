3add672faa79118902cb2a39ad978d4c06d26992
koha11 <khoa88108@gmail.com>
2025-04-28 07:11:41 +0700

    feat add orderDetails query method

--- src/main/java/io/github/koha11/pizza_store_pos/repository/OrderDetailRepository.java
+++ src/main/java/io/github/koha11/pizza_store_pos/repository/OrderDetailRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, String> {
     @Query("SELECT od FROM OrderDetail od WHERE od.orderId = :orderId")
