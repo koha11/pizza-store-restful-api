bb210b3642cb308a9bc19ad79b933cddb5541e00
koha11 <khoa88108@gmail.com>
2025-05-03 10:38:16 +0700

    fix change seat order feature

--- src/main/java/io/github/koha11/pizza_store_pos/repository/OrderRepository.java
+++ src/main/java/io/github/koha11/pizza_store_pos/repository/OrderRepository.java
@@ -12,7 +12,7 @@ public interface OrderRepository extends JpaRepository<Order, String> {
     @Query("SELECT o from Order o where o.seatId = :seatId and o.status = 0 ORDER BY o.createdAt")
     Optional<Order> findBySeatId(String seatId);
 
-    @Query("SELECT o from Order o where o.orderId like '%dateString%' ORDER BY o.createdAt  ")
+    @Query("SELECT o from Order o where o.orderId like %:dateString% ORDER BY o.createdAt  ")
     List<Order> findAllByDateString(@Param("dateString") String dateString);
 
     @Query("SELECT o from Order o where o.status = 1 ORDER BY o.createdAt")
