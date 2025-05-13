a5acb0d3c2caaa427bd3f340cd67846f0e44a747
koha11 <khoa88108@gmail.com>
2025-05-13 12:45:29 +0700

    fix add new food type

--- src/main/java/io/github/koha11/pizza_store_pos/config/FoodConfig.java
+++ src/main/java/io/github/koha11/pizza_store_pos/config/FoodConfig.java
@@ -18,11 +18,13 @@ public class FoodConfig {
         return args -> {
             FoodType pizza = new FoodType("FT0001", "Pizza", "https://ilmio.vn/uploads/fnb-menu/pizza/giant-pizza-size42cm.jpg", Timestamp.valueOf(LocalDateTime.now()));
 
+            FoodType desert = new FoodType("FT0002", "Desert", "https://ilmio.vn/uploads/fnb-menu/pizza/giant-pizza-size42cm.jpg", Timestamp.valueOf(LocalDateTime.now()));
+
             Food peperoni = new Food("F00001", "FT0001", "Peperoni", "https://ilmio.vn/uploads/fnb-menu/pizza/pizza-pepperoni.jpg","Italian sausages, mozzarella.", 135000, Timestamp.valueOf(LocalDateTime.now()));
 
             Food margherita = new Food("F00002", "FT0001", "Margherita", "https://ilmio.vn/uploads/fnb-menu/pizza/pizza-margherita.jpg", "A classic pizza originating in Naples Italy: Tomato sauce and mozzarella.", 98000, Timestamp.valueOf(LocalDateTime.now()));
 
-            foodTypeRepo.save(pizza);
+            foodTypeRepo.saveAll(List.of(pizza, desert));
             foodRepo.saveAll(List.of(peperoni, margherita));
         };
     }
