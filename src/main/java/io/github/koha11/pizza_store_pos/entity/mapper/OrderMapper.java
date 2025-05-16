8f6eca1aa0c4e2c6bff72b46ca899c1f56609d33
koha11 <khoa88108@gmail.com>
2025-05-16 19:02:52 +0700

    add getOrderByOrderId method and change order mapper

--- src/main/java/io/github/koha11/pizza_store_pos/entity/mapper/OrderMapper.java
+++ src/main/java/io/github/koha11/pizza_store_pos/entity/mapper/OrderMapper.java
@@ -2,23 +2,32 @@ package io.github.koha11.pizza_store_pos.entity.mapper;
 
 import io.github.koha11.pizza_store_pos.entity.employee.Employee;
 import io.github.koha11.pizza_store_pos.entity.order.OnTableOrder;
+import io.github.koha11.pizza_store_pos.entity.order.OnTableOrderDetail;
 import io.github.koha11.pizza_store_pos.entity.order.Order;
 import io.github.koha11.pizza_store_pos.entity.order.OrderStatistic;
 import io.github.koha11.pizza_store_pos.service.EmployeeService;
+import io.github.koha11.pizza_store_pos.service.OrderDetailService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public abstract class OrderMapper {
     @Autowired
     protected EmployeeService employeeService;
 
+    @Autowired
+    protected OrderDetailService orderDetailService;
+
+    @Mapping(source = "orderId", target = "foods", qualifiedByName = "getFoods")
     public abstract OnTableOrder orderToDTO(Order order);
 
     @Mapping(source = "serverId", target = "serverName", qualifiedByName = "getEmployeeName")
     @Mapping(source = "cashierId", target = "cashierName", qualifiedByName = "getEmployeeName")
+    @Mapping(source = "orderId", target = "foods", qualifiedByName = "getFoods")
     public abstract OrderStatistic orderToStatistic(Order order);
 
     public abstract Order DTOToOrder(OnTableOrder onTableOrder);
@@ -28,5 +37,10 @@ public abstract class OrderMapper {
         if (employeeId == null) return null;
         Employee emp = employeeService.getOne(employeeId);
         return emp.getFullName();
+    }
+
+    @Named("getFoods")
+    protected List<OnTableOrderDetail> getFoods(String orderId) {
+        return orderDetailService.getAllByOrderId(orderId);
     }
 }
