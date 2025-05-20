74d872dd8381fd7bb1319fe1bc69b224cf3a44fb
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-21 00:06:48 +0700

    feat add service to init & get salary sheet

---
+++ src/main/java/io/github/koha11/pizza_store_pos/service/SalaryService.java
@@ -0,0 +1,82 @@
+package io.github.koha11.pizza_store_pos.service;
+
+import io.github.koha11.pizza_store_pos.entity.employee.EmpType;
+import io.github.koha11.pizza_store_pos.entity.employee.Employee;
+import io.github.koha11.pizza_store_pos.entity.salary.Salary;
+import io.github.koha11.pizza_store_pos.entity.timesheet.Timesheet;
+import io.github.koha11.pizza_store_pos.entity.violation.ViolationRecord;
+import io.github.koha11.pizza_store_pos.repository.EmployeeRepository;
+import io.github.koha11.pizza_store_pos.repository.SalaryRepository;
+import io.github.koha11.pizza_store_pos.repository.TimesheetRepository;
+import io.github.koha11.pizza_store_pos.repository.ViolationRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Service
+public class SalaryService {
+
+    @Autowired
+    private EmployeeRepository employeeRepo;
+
+    @Autowired
+    private SalaryRepository salaryRepo;
+
+    @Autowired
+    private TimesheetRepository timesheetRepo;
+
+    @Autowired
+    private ViolationRecordRepository violationRecordRepo;
+
+    public void initSalarySheet(Month month, int year){
+        List<Salary> existingSalary = salaryRepo.findAllByMonthYear(month, year);
+        if(!existingSalary.isEmpty()) {
+            return;
+        }
+        LocalDate startDate = LocalDate.of(year,month, 1);
+        LocalDate endDate = LocalDate.of(year,month, YearMonth.of(year, month).lengthOfMonth());
+        List<Timesheet> timesheets = timesheetRepo.findAllByMonth(startDate, endDate);
+        Map<String, Salary> salaryMap = new HashMap<>();
+        for(Timesheet ts : timesheets) {
+            List<ViolationRecord> violationRecords = violationRecordRepo.findAllByEmpId(ts.getEmpId(),startDate,endDate);
+            Salary salary = salaryMap.get(ts.getEmpId());
+            Employee emp = employeeRepo.findById(ts.getEmpId()).orElse(null);
+            if(salary == null) {
+                salary = new Salary();
+                salaryMap.put(ts.getEmpId(), salary);
+                salary.setEmpId(ts.getEmpId());
+                salary.setEmpType(emp.getEmpType());
+            }
+            salary.setEmpName(emp.getFullName());
+            salary.setMonth(month);
+            salary.setYear(year);
+            if(ts.isStatus()) {
+                if(ts.getWorkingHours() > 0) {
+                    salary.setTotalWorkingDay(salary.getTotalWorkingDay() + 1);
+
+                } else {
+                    salary.setTotalOffDay(salary.getTotalOffDay() + 1);
+                }
+            }
+            int totalFine = violationRecords.stream()
+                    .mapToInt(vr -> vr.getViolation().getViolationFine())
+                    .sum();
+            salary.setTotalFine(totalFine);
+            int totalSalary = salary.getEmpType().getBasicSalary() + salary.getTotalOvertimeWorkingHours() - salary.getTotalFine();
+            salary.setTotalSalary(totalSalary);
+        }
+        salaryRepo.saveAll(new ArrayList<>(salaryMap.values()));
+    }
+
+    public List<Salary> getSalaryByMonthYear(Month month, int year) {
+        return salaryRepo.findAllByMonthYear(month, year);
+    }
+}
