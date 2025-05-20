835e29f202308197cf33be2b317bc1c58bf01576
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-21 00:05:26 +0700

    feat add entity salary sheet to calculate salary each employee

---
+++ src/main/java/io/github/koha11/pizza_store_pos/entity/salary/Salary.java
@@ -0,0 +1,33 @@
+package io.github.koha11.pizza_store_pos.entity.salary;
+
+import io.github.koha11.pizza_store_pos.entity.employee.EmpType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Month;
+
+@Table(name = "salary")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@IdClass(SalaryId.class)
+public class Salary {
+    @Id
+    private String empId;
+    private String empName;
+    @ManyToOne
+    private EmpType empType;
+    @Id
+    private Month month;
+    @Id
+    private int year;
+    private int totalWorkingDay;
+    private int totalOvertimeWorkingHours;
+    private int totalFine;
+    private int totalOffDay;
+    private int totalSalary;
+}
+
