2ff8a354ea0490b9201d74328f8136060e507101
koha11 <khoa88108@gmail.com>
2025-06-02 14:10:42 +0700

    fix get reservable tables (check only unfinished reservation)

--- src/main/java/io/github/koha11/pizza_store_pos/service/TableService.java
+++ src/main/java/io/github/koha11/pizza_store_pos/service/TableService.java
@@ -59,7 +59,7 @@ public class TableService extends GenericService<StoreTable>{
         List<TableReservation> tableReservations = tableReservationService.getTableReservationsByDateAndTable(reservedDate, "");
 
         // Lay ra nhung phieu dat ban co thoi gian chenh lech it hon 3 tieng so voi thoi gian dinh tao phieu va co seatId khac ""
-        List<String> sameSeatList = tableReservations.stream().filter(tableReservation -> Duration.between(tableReservation.getBookedTime(), reservedDate).getSeconds() <= 3600*3 && !tableReservation.getTableId().isEmpty()).map(TableReservation::getTableId).toList();
+        List<String> sameSeatList = tableReservations.stream().filter(tableReservation -> Duration.between(tableReservation.getBookedTime(), reservedDate).getSeconds() <= 3600*3 && !tableReservation.getTableId().isEmpty() && tableReservation.getStatus() == ReservationStatus.UNFINISHED).map(TableReservation::getTableId).toList();
 
         // Loc de lay ra so ban khong bi trung lich dat, co so luong cho ngoi thoa man voi so luong khach du kien
         return storeTables.stream().filter(storeTable -> !sameSeatList.contains(storeTable.getTableId()) && storeTable.getSeats() >= dinerCount).toList();
