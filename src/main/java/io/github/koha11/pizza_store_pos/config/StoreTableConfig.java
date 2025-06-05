75b42452186506669a24d45d642d68b05de92099
koha11 <khoa88108@gmail.com>
2025-06-05 20:01:23 +0700

    init table data

--- src/main/java/io/github/koha11/pizza_store_pos/config/StoreTableConfig.java
+++ src/main/java/io/github/koha11/pizza_store_pos/config/StoreTableConfig.java
@@ -19,9 +19,15 @@ public class StoreTableConfig {
             StoreTable t01 = new StoreTable("T01", TableStatus.AVAILABLE, 4, Timestamp.valueOf(LocalDateTime.now().plusMinutes(1)));
             StoreTable t02 = new StoreTable("T02", TableStatus.AVAILABLE, 2, Timestamp.valueOf(LocalDateTime.now().plusMinutes(2)));
             StoreTable t03 = new StoreTable("T03", TableStatus.AVAILABLE, 4, Timestamp.valueOf(LocalDateTime.now().plusMinutes(3)));
-            StoreTable t04 = new StoreTable("T04", TableStatus.UNAVAILABLE, 8, Timestamp.valueOf(LocalDateTime.now().plusMinutes(4)));
+            StoreTable t04 = new StoreTable("T04", TableStatus.AVAILABLE, 6, Timestamp.valueOf(LocalDateTime.now().plusMinutes(4)));
+            StoreTable t05 = new StoreTable("T05", TableStatus.AVAILABLE, 4, Timestamp.valueOf(LocalDateTime.now().plusMinutes(5)));
+            StoreTable t06 = new StoreTable("T06", TableStatus.AVAILABLE, 6, Timestamp.valueOf(LocalDateTime.now().plusMinutes(6)));
+            StoreTable t07 = new StoreTable("T07", TableStatus.UNAVAILABLE, 4, Timestamp.valueOf(LocalDateTime.now().plusMinutes(7)));
+            StoreTable t08 = new StoreTable("T08", TableStatus.AVAILABLE, 4, Timestamp.valueOf(LocalDateTime.now().plusMinutes(8)));
+            StoreTable t09 = new StoreTable("T09", TableStatus.AVAILABLE, 2, Timestamp.valueOf(LocalDateTime.now().plusMinutes(9)));
+            StoreTable t10 = new StoreTable("T10", TableStatus.AVAILABLE, 8, Timestamp.valueOf(LocalDateTime.now().plusMinutes(10)));
 
-            tableRepo.saveAll(List.of(t01, t02, t03, t04));
+            tableRepo.saveAll(List.of(t01, t02, t03, t04, t05, t06, t07, t08, t09, t10));
         };
     }
 }
